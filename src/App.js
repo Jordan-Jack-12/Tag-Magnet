71a3b532b023258199745081605cb1b07a041750
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2023-05-29 11:04:09 +0530

    updated to only search endpoint

--- src/App.js
+++ src/App.js
@@ -1,95 +1,57 @@
 import { useEffect, useState } from "react";
-import { Heading, Center } from '@chakra-ui/react';
+import { Heading, Center, Button, FormLabel, Input } from '@chakra-ui/react';
 
-import Search from './Search';
-import Top from './Top';
-import Trending from "./Trending";
 import Hashtags from "./Hashtags";
 
 function App() {
-
-  const [hashtag, setHashtag] = useState([]);
-  const [trending, setTrending] = useState([]);
-  const [top, setTop] = useState([]);
-  const [search, setSearch] = useState([]);
   const [formSubmitted, setFormSubmitted] = useState(false);
-
-  const fetchTrending = async () => {
-    const url = 'https://hashtag5.p.rapidapi.com/api/v2.1/tag/trending';
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'c9199fe501mshe74942524fa8c78p16e6a7jsnae60956a3a9c',
-        'X-RapidAPI-Host': 'hashtag5.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setTrending(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const fetchTop = async () => {
-    const url = 'https://hashtag5.p.rapidapi.com/api/v2.1/tag/top';
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'c9199fe501mshe74942524fa8c78p16e6a7jsnae60956a3a9c',
-        'X-RapidAPI-Host': 'hashtag5.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setTop(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+  const [hashtag, setHashtag] = useState([]);
+  const [search, setSearch] = useState([]);
   
-  const fetchSeacrh = async() =>{
-    const url = `https://hashtag5.p.rapidapi.com/api/v2.1/tag/predict?keyword=${search}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'c9199fe501mshe74942524fa8c78p16e6a7jsnae60956a3a9c',
-        'X-RapidAPI-Host': 'hashtag5.p.rapidapi.com'
-      }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setHashtag(result);
-    } catch (error) {
-      console.error(error);
+    if (formSubmitted === false){
+      const fetchSeacrh = async() =>{
+        const url = `https://hashtag5.p.rapidapi.com/api/v2.1/tag/predict?keyword=${search}`;
+        const options = {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': 'c9199fe501mshe74942524fa8c78p16e6a7jsnae60956a3a9c',
+            'X-RapidAPI-Host': 'hashtag5.p.rapidapi.com'
+          }
+        };
+    
+        try {
+          const response = await fetch(url, options);
+          const result = await response.json();
+          setHashtag(result);
+          console.log(result)
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchSeacrh();
     }
-  };
+    setFormSubmitted(true);
+    setTimeout(()=>{
+      setFormSubmitted(false);
+    }, 2000);
+    console.log(formSubmitted);
+  }
 
-  if (formSubmitted){
-    fetchSeacrh();
-    setFormSubmitted(false);
-  };
-
-  useEffect(()=>{
-    fetchTop();
-    fetchTrending();
-  }, [trending, top, formSubmitted, hashtag]);
+  useEffect(()=>{ }, []);
 
   return (
     <Center>
       <div className="App">
           <Heading bg="blue.500" color="white">Hash Tag Finder</Heading>
           <Hashtags hashtag={hashtag} />
-          <Search search={search} setSearch={setSearch} setFormSubmitted={setFormSubmitted} />
-          <Trending trending={trending} />
-          <Top top={top} />
+          <form onSubmit={handleSubmit}>
+            <FormLabel>Search for hashtags</FormLabel>
+            <Input placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)} />
+            <Button colorScheme='blue' type="submit">Search</Button>
+          </form>
           <footer>Footer</footer>
       </div>
     </Center>
--- src/Hashtags.js
+++ src/Hashtags.js
@@ -1,28 +1,25 @@
 import React from 'react'
 
-const Hashtags = () => {
-    // if (hashtags !== [] && hashtags !== null){
-    //     return (
-    //         <div>
-    //             {Object.entries(hashtags).map(([key, Array])=>(
-    //                 <div key={key}>
-    //                     <ul>
-    //                         {Array.map((index, item)=>(
-    //                             <li key={index}>#{item}</li>
-    //                         ))}
-    //                     </ul>
-    //                 </div>
-    //             ))}
-    //         </div>
-    //       )
-    // }else{
-    //     return(
-    //         <div></div>
-    //     )
-    // }
-    return(
-        <div></div>
-    )
+const Hashtags = ({ hashtag }) => {
+    if (hashtag !== [] && hashtag !== null){
+        return (
+            <div>
+                {Object.entries(hashtag).map(([key, array])=>(
+                    <div key={key}>
+                        <ul>
+                            {array.map((item, index)=>(
+                                <li key={index}>#{item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+          )
+    }else{
+        return(
+            <div></div>
+        )
+    }
 
 }
 
